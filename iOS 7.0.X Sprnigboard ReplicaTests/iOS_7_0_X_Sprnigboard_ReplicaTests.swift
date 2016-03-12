1c3496206fe80d809b698c421bdc52c549c54630
나김준우 <Joonwoo@imac.unet.ca>
2016-03-12 15:20:17 -0800

    Initial Commit

---
+++ iOS 7.0.X Sprnigboard ReplicaTests/iOS_7_0_X_Sprnigboard_ReplicaTests.swift
@@ -0,0 +1,36 @@
+//
+//  iOS_7_0_X_Sprnigboard_ReplicaTests.swift
+//  iOS 7.0.X Sprnigboard ReplicaTests
+//
+//  Created by 김준우 on 2016. 3. 12..
+//  Copyright © 2016년 김준우. All rights reserved.
+//
+
+import XCTest
+@testable import iOS_7_0_X_Sprnigboard_Replica
+
+class iOS_7_0_X_Sprnigboard_ReplicaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
